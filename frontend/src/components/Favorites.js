e829adab4bca57dd48dd583c534baffe47938451
AjnasNB <ajnasnb@gmail.com>
2024-06-07 18:10:44 +0530

    updated some minor elements in fav

--- frontend/src/components/Favorites.js
+++ frontend/src/components/Favorites.js
@@ -1,27 +1,39 @@
 // src/components/Favorites.js
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoIosArrowDropleft } from "react-icons/io";
+
+import './Favorites.css';
 import MovieCard from './MovieCard';
-import './Favorites.css'; // Create a CSS file for favorites styles
 
 const Favorites = () => {
-  const [favorites, setFavorites] = useState([]);
+  const navigate = useNavigate();
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-  useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    setFavorites(storedFavorites);
-  }, []);
+  const handleBackClick = () => {
+    navigate('/');
+  };
 
   return (
-    <div className="favorites">
-      <h2>My Favorites</h2>
-      <div className="favorite-movies">
-        {favorites.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+    <div className="favorites-container">
+      <div className="favorites-header">
+        <button className="back-button" onClick={handleBackClick}>
+          <IoIosArrowDropleft className="back-icon" />
+        </button>
+        <h1>My Favourites</h1>
+      </div>
+      <div className="favorites-list">
+        {favorites.length === 0 ? (
+          <p>You have no favorite movies.</p>
+        ) : (
+          favorites.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Favorites;
